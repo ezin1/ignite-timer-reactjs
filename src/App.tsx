b3cdd7168483faba7c6d7d1302e2be07e75f4472
ezin1 <eziof.dev@gmail.com>
2024-10-28 00:14:00 -0300

    feat: create initial configs for Ignite Timer and initial concepts for styled components

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import {ThemeProvider} from "styled-components";
+
+import { Button } from "./components/Button";
+import { defaultTheme } from "./styles/themes/default";
+
+export function App() {
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary"></Button>
+      <Button variant="secondary"></Button>
+      <Button variant="danger"></Button>
+      <Button variant="success"></Button>
+      <Button></Button>
+    </ThemeProvider>
+  )
+}
+
